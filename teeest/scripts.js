647098aa90c076676b47d41b8e03a02a4cbf2c47
jamicat <ahrlyyy@gmail.com>
2025-07-20 02:48:38 +0100

    Update scripts.js

--- teeest/scripts.js
+++ teeest/scripts.js
@@ -289,8 +289,8 @@ function showGuestBook() {
       <p>Welcome to the Guestbook! Be nice.</p>
     </div>
     <form class="space-y-4 text-white">
-      <input type="text" placeholder="Name" class="w-full p-2 bg-black bg-opacity-20 border border-pink-600 rounded border-opacity-75 required" />
-      <textarea placeholder="Message!" class="w-full p-2 bg-black bg-opacity-20 border border-pink-600 rounded border-opacity-75 required"></textarea>
+      <input type="text" placeholder="Name" class="w-full p-2 bg-black bg-opacity-20 border border-pink-600 rounded border-opacity-75" required />
+      <textarea placeholder="Message!" class="w-full p-2 bg-black bg-opacity-20 border border-pink-600 rounded border-opacity-75" required></textarea>
       <div class="text-center">
         <button type="submit" class="terminal-button">Submit</button>
       </div>
