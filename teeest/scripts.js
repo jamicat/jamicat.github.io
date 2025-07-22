e9922171a647522409bbd540b5288c2a1cabe98c
jamicat <ahrlyyy@gmail.com>
2025-07-22 21:03:13 +0100

    Update scripts.js

--- teeest/scripts.js
+++ teeest/scripts.js
@@ -291,7 +291,7 @@ function showGuestBook() {
   <div class="drag-area text-pink-600 text-sm mb-2 select-none flex justify-between items-center">
     <span class="flex items-center space-x-2">
       <img src="jami2.png" alt="Avatar2" class="avatar-icon" />
-     <span id="typed" class="text-pink-600 text-lg mt-4 mb-4"></span>
+     <span id="typed2" class="text-pink-600 text-lg mt-4 mb-4"></span>
     </span>
     <div class="flex items-center space-x-2 mr-3 -mt-12">
       <button onclick="closeGuestBook()" class="text-red-300 hover:text-red-400 transition-colors duration-200 text-lg leading-none">
@@ -303,7 +303,6 @@ function showGuestBook() {
   </div>
 
   <div class="flex flex-col sm:flex-row gap-6">
-    <!-- Form Area -->
     <div class="w-full">
       <div class="text-pink-300 text-md mt-2 mb-4 text-center">
         <p class="text-base">Welcome! Be nice.</p>
@@ -319,7 +318,7 @@ function showGuestBook() {
   </div>
 `;
 
-var typedTitle = new Typed('#typed', {
+var typed2= new Typed('#typed2', {
   strings: ['Guestbook'],
   typeSpeed: 80,
   showCursor: true,
