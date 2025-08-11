fa9e2752b11e575d6080403809bc5608acf58dab
jamicat <ahrlyyy@gmail.com>
2025-08-11 13:34:33 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -171,14 +171,14 @@ const canvas = document.getElementById('canvas');
 
 if (galaxyVisible) {
 galaxyVisible = false;
-rewind10.classList.remove('text-white');
-rewind10.classList.add('text-pink-600');
+rewind10.classList.remove('text-purple-300');
+rewind10.classList.add('text-purple-50');
 canvas.style.opacity = '0';
 canvas.style.pointerEvents = 'none'; 
 } else {
 galaxyVisible = true;
-rewind10.classList.add('text-white');
-rewind10.classList.remove('text-pink-600');
+rewind10.classList.add('text-purple-300');
+rewind10.classList.remove('text-purple-50');
 canvas.style.opacity = '1';
 canvas.style.pointerEvents = 'auto';  
 }
@@ -681,6 +681,7 @@ loop: true,
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
+
 
 
 
