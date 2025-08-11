b85d7ed616f18b0d42c153a1de3d4761463564e5
jamicat <ahrlyyy@gmail.com>
2025-08-11 22:20:55 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -244,9 +244,6 @@ function showArt() {
 $('#terminalContent').html(`
 <div class="text-pink-300 text-lg mb-4 mt-4"></div>
   <div id="artGallery" class="grid grid-cols-3 gap-4">
-   <a href="closeup.jpg" class="block rounded overflow-hidden">
-    <img src="closeup_thumb.jpg" alt="Jamie - Oliv" class="rounded hover:scale-105 transition transform duration-200" />
-      </a>
   <a href="festival.jpg" class="block rounded overflow-hidden">
     <img src="festival_thumb.jpg" alt="Jamie - Festival" class="rounded hover:scale-105 transition transform duration-200" />
       </a>
@@ -681,6 +678,7 @@ loop: true,
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
+
 
 
 
