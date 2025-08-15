61bf81e1158a3b8ffda1890348715a171538b129
jamicat <ahrlyyy@gmail.com>
2025-08-15 23:00:47 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -297,7 +297,7 @@ $('#terminalContent').html(`
   <p>
     Now go and click on something, or nothing. 
     <span class="text-pink-glow text-white relative inline-block group cursor-pointer">
-    Thanks for visiting!
+    >Thanks for visiting!
     <img src="thankyou.png" alt="Thank you!"
          class="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 w-[300px] h-[311px] max-w-none z-50 rounded shadow-lg opacity-0 invisible transition-opacity duration-150 group-hover:opacity-100 group-hover:visible pointer-events-none"/>
   </span>
@@ -707,6 +707,7 @@ loop: true,
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
+
 
 
 
