948bd8d653ca731bc864f1bf1c5c3e68fa16f1d6
jamicat <ahrlyyy@gmail.com>
2025-08-21 16:30:10 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -66,7 +66,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: 'ZM_Gamwxvtc,9DiJJVfZXUk',
+playlist: 'oJZmO5mByVY,9DiJJVfZXUk',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -107,7 +107,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = 'ZM_Gamwxvtc';
+const videoId = 'oJZmO5mByVY';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -190,10 +190,10 @@ rewind10.click();
 function showList() {
 const playlist = [
 {
-title: "Cavetown - This Is Home",
-videoId: "ZM_Gamwxvtc",
-channelAvatar: "https://yt3.googleusercontent.com/oWWGvBTDEGOyU2IkWe9ycVjpyHkvIhgNBJjKfWx4WfIXWYtIZB0L-oEahLKRZ-boo3gt-FwUlg=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@cavetown"
+title: "Changes (Live From Villa Park)",
+videoId: "oJZmO5mByVY",
+channelAvatar: "https://yt3.googleusercontent.com/nYTKh5VVSOqq9vK5CoEQY8HWlMxKshHQ0H_eM0lBraA7YtQqwDavCFTYRUHH5DG07SWwRNfn=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@YUNGBLUD"
 },
 {
 title: "Cavetown - Baby Spoon",
@@ -717,6 +717,7 @@ loop: true,
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
+
 
 
 
