3639262daa12acab633c1e8cbadeadcb1d8a001d
jamicat <ahrlyyy@gmail.com>
2025-09-05 22:01:09 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -74,7 +74,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: '_D5nHQTxDl0',
+playlist: '9NyDmd9xRgI',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -115,7 +115,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = '_D5nHQTxDl0';
+const videoId = '9NyDmd9xRgI';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -198,10 +198,10 @@ canvas.style.pointerEvents = 'auto';
 function showList() {
 const playlist = [
 {
-title: "cavetown",
-videoId: "_D5nHQTxDl0",
-channelAvatar: "https://yt3.googleusercontent.com/oWWGvBTDEGOyU2IkWe9ycVjpyHkvIhgNBJjKfWx4WfIXWYtIZB0L-oEahLKRZ-boo3gt-FwUlg=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@cavetown"
+title: "Cavetown - Devil Town",
+videoId: "9NyDmd9xRgI",
+channelAvatar: "https://yt3.googleusercontent.com/ytc/AIdro_k5bmfSa10v89Xnbf9axAnTSel2cOkmuBHPwpAG4EBvCA=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@lemonboy4954"
 }
 ];
 
@@ -735,6 +735,7 @@ loop: true,
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
+
 
 
 
