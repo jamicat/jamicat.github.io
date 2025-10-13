4be1a6bb26cf51611f077a096ccac6f2863087c8
jamicat <ahrlyyy@gmail.com>
2025-10-13 19:37:17 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#typed', {
 strings: [
-'<span class="text-white text-base mr-2 text-blue-glow">Myeoooooow, welcome!</span>',
+'<span class="text-white text-base mr-2 text-blue-glow">Meoeoeoeoeow, welcome!</span>',
 '<span class="text-white text-base mr-2 text-blue-glow">Click on the galaxy icon!</span>',
 '<span class="text-white text-base mr-2 text-blue-glow">Check out the guestbook!</span>',
 '<span class="text-white text-base mr-2 text-blue-glow">ฅ(^ω^)ฅ</span>',
@@ -798,6 +798,7 @@ loop: true,
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.head.appendChild(tag);
+
 
 
 
