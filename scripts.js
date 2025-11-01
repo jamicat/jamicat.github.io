4c1b6b2d32f9507cae90a7e4cf41ac8bc83aae1a
jamicat <ahrlyyy@gmail.com>
2025-11-01 00:09:04 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -165,7 +165,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: '6ZWhNxslxzA,Mfwmxfn3nyM',
+playlist: 'Mfwmxfn3nyM,6ZWhNxslxzA',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -206,7 +206,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = '6ZWhNxslxzA';
+const videoId = 'Mfwmxfn3nyM';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -299,16 +299,16 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
-title: "Jane Remover — Dreamflasher",
-videoId: "6ZWhNxslxzA",
-channelAvatar: "https://yt3.googleusercontent.com/OwNIBpyWkwEtJTXSOuu7ojmBT2QwSZyul20FcTE79yy3R4Awcdns_jrUS-ynWceACPIHNwQLMA=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@jane_remover"
-},
-{
 title: "South Arcade - stone cold summer",
 videoId: "Mfwmxfn3nyM",
 channelAvatar: "https://yt3.googleusercontent.com/1XVSDR-XAhQ6v_djDiG7lHCdk2v1JxiF1lh8FeH0dosLddhuMFfbKgfA-LZXfebNk84hstFgpQ=s160-c-k-c0x00ffffff-no-rj",
 channelUrl: "https://www.youtube.com/@southarcadeuk"
+},
+{
+title: "Jane Remover — Dreamflasher",
+videoId: "6ZWhNxslxzA",
+channelAvatar: "https://yt3.googleusercontent.com/OwNIBpyWkwEtJTXSOuu7ojmBT2QwSZyul20FcTE79yy3R4Awcdns_jrUS-ynWceACPIHNwQLMA=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@jane_remover"
 }
 ];
 
@@ -805,6 +805,7 @@ window.addEventListener('DOMContentLoaded', () => {
   applyTheme(savedTheme);
   initTyped(savedTheme);
 });
+
 
 
 
