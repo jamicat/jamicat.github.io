2a1df6392b5c44f832e75950317c32f134c86f75
jamicat <ahrlyyy@gmail.com>
2026-01-02 18:30:52 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -164,7 +164,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: '3tlzF2jtzEU',
+playlist: 'pf3KyEnacJ8',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -205,7 +205,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = '3tlzF2jtzEU';
+const videoId = 'pf3KyEnacJ8';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -298,8 +298,8 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
-title: "YUNGBLUD - Zombie",
-videoId: "3tlzF2jtzEU",
+title: "YUNGBLUD, The Smashing Pumpkins - Zombie ",
+videoId: "pf3KyEnacJ8",
 channelAvatar: "https://yt3.googleusercontent.com/nYTKh5VVSOqq9vK5CoEQY8HWlMxKshHQ0H_eM0lBraA7YtQqwDavCFTYRUHH5DG07SWwRNfn=s160-c-k-c0x00ffffff-no-rj",
 channelUrl: "https://www.youtube.com/@yungblud"
 }
@@ -916,6 +916,7 @@ window.addEventListener('DOMContentLoaded', () => {
   applyTheme(savedTheme);
   initTyped(savedTheme);
 });
+
 
 
 
