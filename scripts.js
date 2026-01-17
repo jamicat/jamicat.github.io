2219196e337db3b1b013f3911e0e1089f0404220
jamicat <ahrlyyy@gmail.com>
2026-01-17 14:57:13 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -561,7 +561,7 @@ function showGuestBook() {
   <div class="drag-area flex justify-between items-center select-none mb-2 text-sm">
     <span class="flex items-center space-x-2">
       <img src="g2.gif" alt="Avatar2" class="avatar-icon2" />
-      <span id="typed2" class="text-lg mt-4 mb-4 text-blue-glow no-theme-glow">guestbook</span>
+      <span id="typed2" class="tracking-wide text-lg mt-4 mb-4 text-blue-glow no-theme-glow">guestbook</span>
     </span>
     <div class="flex items-center space-x-2 mr-3 -mt-12">
       <button onclick="closeGuestBook()" class="text-gray-400 hover:text-cyan-400 transition-colors duration-200 text-lg leading-none">
@@ -921,6 +921,7 @@ window.addEventListener('DOMContentLoaded', () => {
   applyTheme(savedTheme);
   initTyped(savedTheme);
 });
+
 
 
 
