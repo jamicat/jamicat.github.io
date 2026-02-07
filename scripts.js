833c4068ad602348b5a915ff066cefff70d4f917
jamicat <ahrlyyy@gmail.com>
2026-02-07 02:42:21 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -170,7 +170,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: '46azFIIHz9s,e4j7FjwCCX0',
+playlist: 'vfRHHFn9E9k,46azFIIHz9s',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -304,16 +304,17 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
+title: "Cavetown - No Bark No Bite (Official Audio)",
+videoId: "vfRHHFn9E9k",
+channelAvatar: "https://yt3.googleusercontent.com/RGUMD69glGEkYdGzvG6H3LwJIzCE3ABWAQfEiUuekf_sH31SjBZ17lUjWLbDFhHRxc2Pl7-OpA=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@cavetown"
+}
+,
+{
 title: "South Arcade - HOW 2 GET AWAY WITH MURDER (Official Video)",
 videoId: "46azFIIHz9s",
 channelAvatar: "https://yt3.googleusercontent.com/1XVSDR-XAhQ6v_djDiG7lHCdk2v1JxiF1lh8FeH0dosLddhuMFfbKgfA-LZXfebNk84hstFgpQ=s160-c-k-c0x00ffffff-no-rj",
 channelUrl: "https://www.youtube.com/@southarcadeuk"
-},
-{
-title: "YUNGBLUD - Cruel Kids (Official Visualiser)",
-videoId: "e4j7FjwCCX0",
-channelAvatar: "https://yt3.googleusercontent.com/nYTKh5VVSOqq9vK5CoEQY8HWlMxKshHQ0H_eM0lBraA7YtQqwDavCFTYRUHH5DG07SWwRNfn=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@yungblud"
 }
 ];
 
@@ -930,6 +931,7 @@ window.addEventListener('DOMContentLoaded', () => {
   applyTheme(savedTheme);
   initTyped(savedTheme);
 });
+
 
 
 
