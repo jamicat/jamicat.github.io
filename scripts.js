81175df20e2dd010e061cd2dd82be24c2dabb3d4
jamicat <ahrlyyy@gmail.com>
2026-02-19 15:45:37 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -170,7 +170,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: 'Ejs_acL9RkM,O00fDLCNhTM',
+playlist: 'b-QCLrTN5pE,O00fDLCNhTM',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -211,7 +211,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = 'Ejs_acL9RkM';
+const videoId = 'b-QCLrTN5pE';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -304,10 +304,10 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
-title: "YUNGBLUD feat. Machine Gun Kelly - acting like that",
-videoId: "Ejs_acL9RkM",
-channelAvatar: "https://yt3.googleusercontent.com/nYTKh5VVSOqq9vK5CoEQY8HWlMxKshHQ0H_eM0lBraA7YtQqwDavCFTYRUHH5DG07SWwRNfn=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@yungblud"
+title: "With That (feat. Duke)",
+videoId: "b-QCLrTN5pE",
+channelAvatar: "https://yt3.googleusercontent.com/aDNK7Ot5mqdrLQjlavDCRuFeTglGkEHBIu2pXZhgYMV-eyk1u3az2igH7m3yG_qw9U3nvb12vrs=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@youngthug"
 }
 ,
 {
@@ -931,6 +931,7 @@ window.addEventListener('DOMContentLoaded', () => {
   applyTheme(savedTheme);
   initTyped(savedTheme);
 });
+
 
 
 
