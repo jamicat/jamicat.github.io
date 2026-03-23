0b5a47cb35e9562730a4abc5b9db30d59d251d61
jamicat <ahrlyyy@gmail.com>
2026-03-23 19:24:51 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -169,7 +169,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: 'Q4algeaPdqs',
+playlist: 'fotb02vfks0',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -210,7 +210,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = 'Q4algeaPdqs';
+const videoId = 'fotb02vfks0';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -303,8 +303,8 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
-title: "Cavetown & Frankie Cosmos - magic 8 ball (Official Music Video)",
-videoId: "Q4algeaPdqs",
+title: "Cavetown - Baby Spoon (Acoustic)",
+videoId: "fotb02vfks0",
 channelAvatar: "https://yt3.googleusercontent.com/RGUMD69glGEkYdGzvG6H3LwJIzCE3ABWAQfEiUuekf_sH31SjBZ17lUjWLbDFhHRxc2Pl7-OpA=s160-c-k-c0x00ffffff-no-rj",
 channelUrl: "https://www.youtube.com/@cavetown"
 }
