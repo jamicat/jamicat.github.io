defedcf5e03ce6704eab623a84aaafa601b422ea
jamicat <ahrlyyy@gmail.com>
2026-03-28 12:48:09 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -454,6 +454,7 @@ terminal.classList.add('sm:w-[480px]');
   applyTheme(currentTheme);
 }
 
+/*
 function aboutPost() {
 
 const terminal = document.getElementById('terminal');
@@ -519,7 +520,7 @@ terminal.classList.add('sm:w-[600px]');
 
  const currentTheme = localStorage.getItem('theme') || 'Default';
  applyTheme(currentTheme);
-}
+}*/
 
 function somethingNew() {
   $('#terminalContent').html(`
@@ -894,7 +895,6 @@ terminal.classList.add('sm:w-[480px]');
   $('#terminalContent').html(`
   <div id="typed" class="text-pink-300 text-lg mb-4 mt-4 text-center"></div>
   <div id="buttonRow" class="flex justify-center space-x-4 flex-wrap sm:flex-nowrap">
-    <button class="terminal-button ml-4" onclick="siteFAQ()">Blog!</button>
     <button class="terminal-button ml-2" onclick="showArt()">Art</button>
     <button class="terminal-button ml-5" onclick="showGuestBook()">Guestbook</button>
     <button class="terminal-button" onclick="showList()">Playlist</button>
