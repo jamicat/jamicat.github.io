a93482ecee4740989ffbbe9ee37970b7aa7f0259
jamicat <ahrlyyy@gmail.com>
2026-03-30 15:13:17 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -169,7 +169,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: 'fotb02vfks0,lfb_VxX-ZZA',
+playlist: '4e0efqBbf_A',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -210,7 +210,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = 'fotb02vfks0';
+const videoId = '4e0efqBbf_A';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -303,19 +303,11 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
-title: "Cavetown - Baby Spoon (Acoustic)",
-videoId: "fotb02vfks0",
-channelAvatar: "https://yt3.googleusercontent.com/RGUMD69glGEkYdGzvG6H3LwJIzCE3ABWAQfEiUuekf_sH31SjBZ17lUjWLbDFhHRxc2Pl7-OpA=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@cavetown"
+title: "my dear friends - [an original song by beetlebug]",
+videoId: "4e0efqBbf_A",
+channelAvatar: "https://yt3.googleusercontent.com/zuw1vd2r87ydvGCj3D6jeosObilt47sZaEGZ2X-JsOQXEQHfKokoUKJoKOcJbfWaGxSgTn4abg=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@auribeetlebug"
 }
-,
-{
-title: "Tarmac",
-videoId: "lfb_VxX-ZZA",
-channelAvatar: "https://yt3.googleusercontent.com/RGUMD69glGEkYdGzvG6H3LwJIzCE3ABWAQfEiUuekf_sH31SjBZ17lUjWLbDFhHRxc2Pl7-OpA=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@cavetown"
-}
-
 ];
 
 let html = `<div class="space-y-4 mt-4">`;
