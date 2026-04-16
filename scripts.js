4763cdd60fa1a0761fb692e53cefbb04c0d6396f
jamicat <ahrlyyy@gmail.com>
2026-04-16 12:45:19 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const themes = {
     glowPrimary: 'text-blue-glow',      
     glowSecondary: 'text-pink-glow',    
     buttonColor: 'bg-orange-300 hover:bg-orange-400',
-    buttonTextColor: 'text-black',
+    buttonTextColor: 'text-white',
     iconColor: 'text-orange-400 hover:text-teal-400',
     galaxyActive: 'text-teal-400',
     galaxyInactive: 'text-orange-400',
