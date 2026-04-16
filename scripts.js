b5cb7f86eb0b2c5be3bf9f9e4fd4b68b937bb9f3
jamicat <ahrlyyy@gmail.com>
2026-04-16 11:12:49 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -167,7 +167,7 @@ autoplay: 0,
 mute: 0,
 controls: 0,
 loop: 1,
-playlist: 'wGDdsxgwquc',
+playlist: 'xGRH6MvX2Oo',
 playsinline: 1,
 modestbranding: 1,
 rel: 0,
@@ -208,7 +208,7 @@ iframeEl.style.opacity = '0';
 });
 }
 
-const videoId = 'wGDdsxgwquc';
+const videoId = 'xGRH6MvX2Oo';
 const posterEl = document.getElementById('videoPoster');
 const iframeEl = document.getElementById('background-video-iframe');
 const highRes = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
@@ -301,10 +301,10 @@ const theme = themes[themeName];
 function showList() {
 const playlist = [
 {
-title: "Jane Remover — TURN UP OR DIE (audio)",
-videoId: "wGDdsxgwquc",
-channelAvatar: "https://yt3.googleusercontent.com/OwNIBpyWkwEtJTXSOuu7ojmBT2QwSZyul20FcTE79yy3R4Awcdns_jrUS-ynWceACPIHNwQLMA=s160-c-k-c0x00ffffff-no-rj",
-channelUrl: "https://www.youtube.com/@jane_remover"
+title: "The Warning - Kerosene (Official Video)",
+videoId: "xGRH6MvX2Oo",
+channelAvatar: "https://yt3.googleusercontent.com/POSi7PPa_cl4X6iwHYb0QF_UZ6Y-V8jopvM1F_tTvERCa1fNSW4z69s2R-GGdIeCeGt0BxOJ5w=s160-c-k-c0x00ffffff-no-rj",
+channelUrl: "https://www.youtube.com/@TheWarning"
 }
 ];
 
