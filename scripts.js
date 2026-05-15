00513848aa5198962cd0847e0fbb809c6218b434
jamicat <ahrlyyy@gmail.com>
2026-05-15 20:12:37 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1065,8 +1065,8 @@ terminal.classList.add('sm:w-[480px]');
   $('#terminalContent').html(`
   <div id="typed" class="text-pink-300 text-lg mb-4 mt-4 text-center"></div>
   <div id="buttonRow" class="flex justify-center space-x-4 flex-wrap sm:flex-nowrap">
-    <button class="terminal-button ml-2" onclick="showPictures()">pictures</button>
     <button class="terminal-button ml-2" onclick="showArt()">art</button>
+    <button class="terminal-button ml-2" onclick="showPictures()">pictures</button>
     <button class="terminal-button ml-5" onclick="showGuestBook()">guestbook</button>
     <button class="terminal-button" onclick="showList()">playlist</button>
   </div>
