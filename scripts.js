67182fae9e7138f03c73d766a7f463d1a47cbd8f
jamicat <ahrlyyy@gmail.com>
2026-06-05 10:32:01 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -925,7 +925,7 @@ async function loadGuestbookComments() {
       div.className = 'bg-pink-50 bg-opacity-[0.03] rounded p-3 mb-2 text-sm';
 
 div.innerHTML = `
-  <div class="font-['nintendoh'] text-xs mb-1 font-medium text-white text-blue-glow no-theme-glow break-all">
+  <div class="font-['nintendoh'] text-sm mb-1 font-medium text-white text-blue-glow no-theme-glow break-all">
     ${entry.name || 'Anonymous'}
   </div>
 
@@ -944,7 +944,7 @@ div.innerHTML = `
 
       <div class="mb-1 flex items-center gap-1 font-medium">
   <img class="replyAvatar w-8 h-8 rounded-full shadow-md object-cover" alt="Discord Avatar">
-  <span class="font-['nintendoh'] text-xs text-white text-blue-glow">Jamie</span>
+  <span class="font-['nintendoh'] text-sm text-white text-blue-glow">Jamie</span>
 </div>
 
       <div class="font-['nintendoh'] text-[0.70rem] mb-1 text-pink-100 break-all">
