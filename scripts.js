60c614e1e0c51cc2cfeb34ad947043f53d180bb3
jamicat <ahrlyyy@gmail.com>
2026-06-12 10:32:35 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -734,7 +734,7 @@ function siteFAQ() {
 $('#terminalContent').html(`
       <div class="text-blue-glow text-white theme-body text-sm mb-4 mt-4">libraries used:</div>
       <ul class="list-disc list-inside text-white space-y-1">
-      <li class="text-blue-glow text-white theme-body text-xs">jquery, typed.js, interact.js, tailwind css, youtube iframe api, lightgallery, three.js, cloudflare workers & apis, websockets (live guest wall updates), custom guest wall api, custom playlist api, local storage (theme persistence)</li>
+      <li class="text-blue-glow text-white theme-body text-xs">jquery, typed.js, interact.js, tailwind css, youtube iframe api, lightgallery, three.js, cloudflare workers & apis, websockets (live guest wall updates), sqlite, custom guest wall api, custom playlist api, local storage (theme persistence)</li>
       </ul>
       </div>
       <div id="buttonRow" class="flex justify-center">
