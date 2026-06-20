d54c5d1e7f31f693b2ab328b69c2024e598ba156
jamicat <ahrlyyy@gmail.com>
2026-06-21 00:48:37 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -738,10 +738,10 @@ $('#terminalContent').html(`
 <div class="text-pink-300 text-lg mb-4 mt-4"></div>
   <div id="artGallery" class="grid grid-cols-2 gap-4">
    <a href="2.png" class="block rounded overflow-hidden">
-    <img src="2.png" alt="Jamie - saproena" class="rounded hover:scale-105 transition transform duration-200" />
+    <img src="2.png" alt="jamie - saproena" class="rounded hover:scale-105 transition transform duration-200" />
       </a>
     <a href="anim_jam.gif" class="block rounded overflow-hidden">
-    <img src="anim_thumb.jpg" alt="Jamie - xandy" class="rounded hover:scale-105 transition transform duration-200" />
+    <img src="anim_thumb.jpg" alt="jamie - xandy" class="rounded hover:scale-105 transition transform duration-200" />
       </a>
     </div>
   <div class="mt-4 flex justify-center">
