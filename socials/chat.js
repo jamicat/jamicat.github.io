67a63730e1f8f4a025e5b3f5072e1f9edc6b8aa2
jamicat <ahrlyyy@gmail.com>
2026-07-25 08:09:46 +0100

    Update chat.js

--- socials/chat.js
+++ socials/chat.js
@@ -1227,11 +1227,11 @@ const groupTimestamp =
     !hasValidDate
         ? "--:--"
         : isPreviousDay
-            ? date.toLocaleDateString("en-GB", {
+            ? `${date.toLocaleDateString("en-GB", {
                   day: "2-digit",
                   month: "2-digit",
-                  year: "2-digit"
-              })
+                  year: "numeric"
+              })} ${formattedTime}`
             : formattedTime;
 
 const fullTimestamp =
