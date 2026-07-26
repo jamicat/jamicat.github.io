3130cf5368abfed56d27e747ca49e07132ee8142
jamicat <ahrlyyy@gmail.com>
2026-07-26 13:20:40 +0100

    Update scripts.js

--- socials/scripts.js
+++ socials/scripts.js
@@ -906,14 +906,14 @@ function siteFAQ() {
             <p class="about-text">
               As a cat away from the keyboard, I'm a pretty feminine guy who's
               always felt happiest surrounded by cosy, cute, pastel-coloured
-              things.
+              things, so it's only fitting that my fursona is a little more pastel-toned too.
             </p>
 
             <p class="about-text">
               I love expressing myself, staying comfy, and embracing my
-              feminine side. I'm always experimenting with new aesthetics,
+              feminine side 💅. I'm always experimenting with new aesthetics,
               finding new outfits to obsess over, and collecting new plushies
-              (30+... I don't have a problem).
+              (30+... I don't have a problem :3).
             </p>
           </section>
 
