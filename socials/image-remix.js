6fbc467e375639e78cfa999ba7d690b2534f3458
jamicat <ahrlyyy@gmail.com>
2026-08-04 13:03:14 +0100

    Update image-remix.js

--- socials/image-remix.js
+++ socials/image-remix.js
@@ -172,10 +172,6 @@ this.sourceContext = null;
                             data-jami-remix-emoji-picker
                             hidden
                         ></div>
-
-                        <div class="jami-remix-overlay-hint">
-                            drag to move · corner to resize · top handle to rotate · flip buttons
-                        </div>
                     </aside>
                 </div>
 
