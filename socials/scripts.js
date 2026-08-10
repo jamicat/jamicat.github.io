3f00aabf6cb280ddd51eff40a96e8ab090cd4c5d
jamicat <ahrlyyy@gmail.com>
2026-08-10 12:41:58 +0100

    Update scripts.js

--- socials/scripts.js
+++ socials/scripts.js
@@ -3911,7 +3911,7 @@ $('#terminalContent').html(`
 <div class="text-pink-300 text-lg mb-4 mt-4"></div>
   <div id="artGallery" class="grid grid-cols-3 gap-4">
    <a href="party.jpg" class="block rounded overflow-hidden">
-    <img src="party_thumb.jpg" alt="jamie & nordy" class="rounded hover:scale-105 transition transform duration-200" />
+    <img src="party_thumb.jpg" alt="yaaaypartypopper" class="rounded hover:scale-105 transition transform duration-200" />
       </a>
    <a href="2.png" class="block rounded overflow-hidden">
     <img src="2.png" alt="jamie - saproena" class="rounded hover:scale-105 transition transform duration-200" />
