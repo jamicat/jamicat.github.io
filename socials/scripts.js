443d3e0ae3184eff628895405e842d57448033b0
jamicat <ahrlyyy@gmail.com>
2026-08-10 12:40:18 +0100

    Update scripts.js

--- socials/scripts.js
+++ socials/scripts.js
@@ -3909,7 +3909,10 @@ document.addEventListener('DOMContentLoaded', () => {
 function showArt() {
 $('#terminalContent').html(`
 <div class="text-pink-300 text-lg mb-4 mt-4"></div>
-  <div id="artGallery" class="grid grid-cols-2 gap-4">
+  <div id="artGallery" class="grid grid-cols-3 gap-4">
+   <a href="party.jpg" class="block rounded overflow-hidden">
+    <img src="party_thumb.jpg" alt="jamie & nordy" class="rounded hover:scale-105 transition transform duration-200" />
+      </a>
    <a href="2.png" class="block rounded overflow-hidden">
     <img src="2.png" alt="jamie - saproena" class="rounded hover:scale-105 transition transform duration-200" />
       </a>
