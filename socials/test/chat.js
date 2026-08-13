5d34df7411626a93547b1de173477a09a518df20
jamicat <ahrlyyy@gmail.com>
2026-08-13 23:30:25 +0100

    Update chat.js

--- socials/test/chat.js
+++ socials/test/chat.js
@@ -17803,11 +17803,11 @@ keepTitleBarInViewport() {
     const makePawMarkup = () =>
         '<svg viewBox="0 0 64 64" aria-hidden="true">' +
         '<path class="jami-chat-paw-shell" d="M31.8 4.4C25.6 4.4 21.7 8.6 20.7 14.3C18.1 9.9 13.5 8.4 9.6 10.6C4.7 13.4 3.9 19.8 6.4 25.1C8.1 28.7 11.2 31 14.7 31.7C11.5 35.2 9.8 39.8 10.5 44.7C11.6 52.2 18 58.1 25.3 58.1C28.1 58.1 30.1 56.9 32 55.6C33.9 56.9 35.9 58.1 38.7 58.1C46 58.1 52.4 52.2 53.5 44.7C54.2 39.8 52.5 35.2 49.3 31.7C52.8 31 55.9 28.7 57.6 25.1C60.1 19.8 59.3 13.4 54.4 10.6C50.5 8.4 45.9 9.9 43.3 14.3C42.3 8.6 38.4 4.4 32.2 4.4Z"></path>' +
-        '<ellipse class="jami-chat-paw-pad" cx="16.2" cy="22.0" rx="3.45" ry="5.25" transform="rotate(-21 16.2 22.0)"></ellipse>' +
-        '<ellipse class="jami-chat-paw-pad" cx="27.0" cy="16.2" rx="3.55" ry="5.35" transform="rotate(-7 27.0 16.2)"></ellipse>' +
-        '<ellipse class="jami-chat-paw-pad" cx="37.0" cy="16.2" rx="3.55" ry="5.35" transform="rotate(7 37.0 16.2)"></ellipse>' +
-        '<ellipse class="jami-chat-paw-pad" cx="47.8" cy="22.0" rx="3.45" ry="5.25" transform="rotate(21 47.8 22.0)"></ellipse>' +
-        '<path class="jami-chat-paw-pad" d="M18.4 44.2C18.4 40.8 21.3 38.0 25.4 35.7L32 31.8L38.6 35.7C42.7 38.0 45.6 40.8 45.6 44.2C45.6 47.5 43.0 49.8 39.6 49.8C36.9 49.8 34.4 48.5 32 46.7C29.6 48.5 27.1 49.8 24.4 49.8C21.0 49.8 18.4 47.5 18.4 44.2Z"></path>' +
+        '<ellipse class="jami-chat-paw-pad" cx="16.7" cy="24.6" rx="3.45" ry="5.25" transform="rotate(-20 16.7 24.6)"></ellipse>' +
+        '<ellipse class="jami-chat-paw-pad" cx="27.1" cy="18.0" rx="3.55" ry="5.35" transform="rotate(-7 27.1 18.0)"></ellipse>' +
+        '<ellipse class="jami-chat-paw-pad" cx="36.9" cy="18.0" rx="3.55" ry="5.35" transform="rotate(7 36.9 18.0)"></ellipse>' +
+        '<ellipse class="jami-chat-paw-pad" cx="47.3" cy="24.6" rx="3.45" ry="5.25" transform="rotate(20 47.3 24.6)"></ellipse>' +
+        '<path class="jami-chat-paw-pad" d="M18.1 44.3C18.1 40.7 21.2 37.9 25.3 35.5C28.1 33.9 30.5 32.3 32 31.4C33.5 32.3 35.9 33.9 38.7 35.5C42.8 37.9 45.9 40.7 45.9 44.3C45.9 47.8 43.1 50.2 39.5 50.2C36.9 50.2 34.7 49.1 33.2 47.8C32.5 47.2 31.5 47.2 30.8 47.8C29.3 49.1 27.1 50.2 24.5 50.2C20.9 50.2 18.1 47.8 18.1 44.3Z"></path>' +
         '</svg>';
 
     const pawPalettes = [
